c07f3a211cb44784891e1255a47fa9ff4018762e
codyThompson <cody.thompson22@gmail.com>
2014-03-04 00:05:45 -0700

    Error fixes for parser skeleton.

--- parser/parser.php
+++ parser/parser.php
@@ -1,4 +1,6 @@
 <?php
+require_once("tagnames.php");
+
 /*
  * Constants used exclusively by this class
  */
@@ -12,7 +14,8 @@ class PageParser {
 
     public function parseDocumentFile($filepath) {
         $domDocument = new DOMDocument();
-        $domDocument->loadXMLFile($filepath);
+        $domDocument->preserveWhiteSpace = false;
+        $domDocument->load($filepath);
 
         $headElement = null;
         $bodyElement = null;
@@ -20,7 +23,7 @@ class PageParser {
         $root = $domDocument->documentElement;
         $rootChildren = $root->childNodes;
 
-        validateHeadAndBody($rootChildren);
+        $this->validateHeadAndBody($rootChildren);
 
         $headElement = $rootChildren->item(0);
         $bodyElement = $rootChildren->item(1);
@@ -28,22 +31,22 @@ class PageParser {
 
     private function validateHeadAndBody($rootChildren) {
         if ($rootChildren->length != 2) {
-            throwInvalidError(EXPECTING_ONLY_HEAD_AND_BODY_PARSER_MESSAGE,
+            $this->throwInvalidError(EXPECTING_ONLY_HEAD_AND_BODY_PARSER_MESSAGE,
                 $rootChildren->length);
         }
         if ($rootChildren->item(0)->tagName !== HEAD_HTML_TAG_NAME) {
-            throwInvalidError(ELEMENT_NOT_FOUND_PARSER_MESSAGE,
+            $this->throwInvalidError(ELEMENT_NOT_FOUND_PARSER_MESSAGE,
                 HEAD_HTML_TAG_NAME);
         }
         if ($rootChildren->item(1)->tagName !== BODY_HTML_TAG_NAME) {
-            throwInvalidError(ELEMENT_NOT_FOUND_PARSER_MESSAGE,
+            $this->throwInvalidError(ELEMENT_NOT_FOUND_PARSER_MESSAGE,
                 BODY_HTML_TAG_NAME);
         }
     }
 
     private function throwInvalidError($message) {
-        $formattedMessage = sprintf(func_get_args());
-        throw new InvalidArgumentError($formattedMessage);
+        $formattedMessage = call_user_func_array("sprintf", func_get_args());
+        throw new InvalidArgumentException($formattedMessage);
     }
 }
 ?>
