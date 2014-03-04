7301c47946366dcfbbb62128f56916e0d083a3a6
codyThompson <cody.thompson22@gmail.com>
2014-03-03 21:05:24 -0700

    Initial parser work.

--- parser/parser.php
+++ parser/parser.php
@@ -1,5 +1,11 @@
 <?php
-define("ELEMENT_NOT_FOUND", "No '%s' element was found while parsing the document");
+/*
+ * Constants used exclusively by this class
+ */
+define("ELEMENT_NOT_FOUND_PARSER_MESSAGE",
+    "No '%s' element was found while parsing the document");
+define("EXPECTING_ONLY_HEAD_AND_BODY_PARSER_MESSAGE",
+    "Expecting only head and body elements at top level, found '%d' elements.");
 
 class PageParser {
     //public function __construct()
@@ -13,22 +19,31 @@ class PageParser {
 
         $root = $domDocument->documentElement;
         $rootChildren = $root->childNodes;
-        foreach($rootChildren as $child) {
-            if ($child->tagName == HEAD_HTML_TAG_NAME) {
-                $headElement = $child;
-            } else if ($child->tagName == BODY_HTML_TAG_NAME) {
-                if ($headElement === null) {
-                    throw new InvalidArgumentException(
-                        createErrorMessage(HEAD_HTML_TAG_NOT_FOUND,
-                        );
-                }
-                $bodyElement = $child;
-            }
+
+        validateHeadAndBody($rootChildren);
+
+        $headElement = $rootChildren->item(0);
+        $bodyElement = $rootChildren->item(1);
+    }
+
+    private function validateHeadAndBody($rootChildren) {
+        if ($rootChildren->length != 2) {
+            throwInvalidError(EXPECTING_ONLY_HEAD_AND_BODY_PARSER_MESSAGE,
+                $rootChildren->length);
+        }
+        if ($rootChildren->item(0)->tagName !== HEAD_HTML_TAG_NAME) {
+            throwInvalidError(ELEMENT_NOT_FOUND_PARSER_MESSAGE,
+                HEAD_HTML_TAG_NAME);
+        }
+        if ($rootChildren->item(1)->tagName !== BODY_HTML_TAG_NAME) {
+            throwInvalidError(ELEMENT_NOT_FOUND_PARSER_MESSAGE,
+                BODY_HTML_TAG_NAME);
         }
     }
 
-    private function createErrorMessage($message) {
-        return sprintf(func_get_args());
+    private function throwInvalidError($message) {
+        $formattedMessage = sprintf(func_get_args());
+        throw new InvalidArgumentError($formattedMessage);
     }
 }
 ?>
